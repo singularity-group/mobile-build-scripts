0cb81319f10bff0e5a6d26bd73df5554c89786d6
Troy Lamerton <troy.lamerton@gmail.com>
2021-03-28 17:06:34 +0200

    optionally build with Android Asan

---
+++ Editor/AddressSanitizer/PostprocessAddressSanitizer.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using UnityEditor.Android;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class PostprocessAddressSanitizer : IPostGenerateGradleAndroidProject {
+    public int callbackOrder => 1;
+
+    private string AddressSanitizerDir => 
+        Path.Combine(Path.Combine(Path.GetFullPath("Packages/com.gamingforgood.mobile_build_scripts"),
+        "Editor", "AddressSanitizer"));
+
+    public void OnPostGenerateGradleAndroidProject(string unityLibraryPath) {
+        var launcherDir = Path.GetFullPath(Path.Combine(unityLibraryPath, "..", "launcher"));
+        // \launcher\src\main\resources\lib\armeabi-v7a\wrap.sh
+        var wrapScriptOutput = Path.Combine(launcherDir, "src", "main", "resources", "lib", "armeabi-v7a", "wrap.sh");
+        
+        // create wrap.sh
+        var repoWrapScript = Path.Combine(AddressSanitizerDir, "Android", "wrap.sh");
+        Directory.CreateDirectory(Path.GetDirectoryName(wrapScriptOutput)!);
+        File.Copy(repoWrapScript, wrapScriptOutput, true);
+        
+        // copy Asan libs
+        if (Application.unityVersion != "2019.4.16f1") {
+            // note for later: instead of include in repo, copy from Android NDK included with Unity installation
+            throw new BuildFailedException("Unity version has updated. The Asan libs that would be copied come from android NDK that is packaged" +
+                                           "with Unity 2019.4.16f1. If Unity has updated their NDK version, then the jniLibs/ need to be updated.");
+        }
+
+        var repoAsanJniLibs = Path.Combine(AddressSanitizerDir, "Android", "jniLibs");
+        var outputJniLibs = Path.Combine(launcherDir, "src", "main", "jniLibs");
+        DirectoryUtil.DirectoryCopy(repoAsanJniLibs, outputJniLibs);
+    }
+}
