aeda7b42a32bf059ab256b77b87d0e61eccb3e40
Troy Lamerton <troy.lamerton@gmail.com>
2021-04-07 11:58:32 +0200

    arm64 use defaults and recover properly from crash on armv7 /androidAny

--- Editor/PostprocessAndroidIl2cppMetadata.cs
+++ Editor/PostprocessAndroidIl2cppMetadata.cs
@@ -20,25 +20,35 @@ public class PostprocessAndroidIl2cppMetadata : IPostGenerateGradleAndroidProjec
             if (skipVar == "1") {
                 return;
             }
+            var metadataFile = Path.Combine(unityLibraryPath, "src/main/assets/bin/Data/Managed/Metadata/global-metadata.dat");
+            // for mono builds, the file does not exist
+            if (!File.Exists(metadataFile)) {
+                return;
+            }
+            
             // Move metadata file to split by abi, because in cloud pipelines we run multiple export player jobs.
             // The code that moves the metadata file to where unity looks is in AndroidPlugins/util
             string abiName;
             switch (PlayerSettings.Android.targetArchitectures) {
-                case AndroidArchitecture.ARM64:
-                    abiName = "arm64-v8a";
-                    break;
-                
                 case AndroidArchitecture.ARMv7:
                     abiName = "armeabi-v7a";
+
+                    var metadataAsLib = Path.Combine(unityLibraryPath, $"src/main/jniLibs/{abiName}/libglobal-metadata.so");
+                    Directory.CreateDirectory(Path.GetDirectoryName(metadataAsLib)!);
+                    File.Copy(metadataFile, metadataAsLib);
+                    break;
+                
+                case AndroidArchitecture.ARM64:
+                    abiName = "arm64-v8a";
+                    
+                    // when bundling both archs in gradle project there must be a 64bit lib for every 32bit lib
+                    var metadataDummyLib = Path.Combine(unityLibraryPath, $"src/main/jniLibs/{abiName}/libglobal-metadata.so");
+                    Directory.CreateDirectory(Path.GetDirectoryName(metadataDummyLib)!);
+                    // create dummy file (might have to be > 0 bytes)
+                    File.WriteAllBytes(metadataDummyLib, new byte[]{ 0x00, 0x00, 0x00, 0x00 });
                     break;
                 
                 default: throw new NotSupportedException("ALL architecture not supported because of buzzing");
-            }
-            var from = Path.Combine(unityLibraryPath, "src/main/assets/bin/Data/Managed/Metadata/global-metadata.dat");
-            if (File.Exists(from)) {
-                var to = Path.Combine(unityLibraryPath, $"src/main/jniLibs/{abiName}/libglobal-metadata.so");
-                Directory.CreateDirectory(Path.GetDirectoryName(to)!);
-                File.Move(from, to);
             }
         } catch (BuildFailedException) {
             throw;
